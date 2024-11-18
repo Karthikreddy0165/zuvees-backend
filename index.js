ca8f2df20f58d324e14592513a54e192e59f90f7
Karthikreddy0165 <Karthikredddy0165@gmail.com>
2024-11-18 15:05:35 +0530

    pull

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-console.log(process.env.PORT);
-console.log(process.env.DATABASE_URL)
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
