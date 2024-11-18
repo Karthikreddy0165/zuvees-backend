4ea50c158e8a9c7ba7e93551e133657c049c61bf
Karthik Reddy <Karthikreddy0165@gmail.com>
2024-11-18 14:38:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ console.log(process.env.PORT);
 console.log(process.env.DATABASE_URL)
 const PORT = process.env.PORT || 5001;
 
+app.get('/',(req,res)=>{
+    res.send("Welcome to zuvees")
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
